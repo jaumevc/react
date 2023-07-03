147189e02c02f9301e79547b252f4cc2534176a8
jaumevc <jaumevc@me.com>
2023-07-03 13:44:25 +0200

    video 7 variables i constants

--- 1-intro-js/src/main.js
+++ 1-intro-js/src/main.js
@@ -1,1 +1,25 @@
+console.log("proves inicials js. VARIABLES!!");
+//PER DECLARAR UNA VARIABLE ANTIGAMENT S'USAVA VAR ara usarem LET o CONST.
+//EL MOTIU ES XQ VAR DEFINEIX UNA VARIABLE D'SCOPE GLOBAL. EN CANVI LET TE UN SCOPE LOCAL.
+
+let nom ="Jaume v.";
+
+let nom2 = 'Susana J.';
+
+nom = 'Mariona';
+
+const lastname= 'Valls';
+
+//lastname ='Jiménez';
+
+const condicio = true;
+
+if(condicio){
+    let nom ='Ares';
+    const lastname = 'Campistany';
+    console.log(nom+' '+lastname);
+}
+
+console.log("Hola món "+nom+' i '+nom2+' '+lastname);
+
 
