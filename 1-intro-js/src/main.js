60a0e9d8dcf518456e4f47e92435c8bba2786038
jaumevc@me.com <jaumevc@me.com>
2023-07-03 18:26:37 +0200

    video 9

--- 1-intro-js/src/main.js
+++ 1-intro-js/src/main.js
@@ -1,16 +1,16 @@
 
-function sayHello(){
+function sayHello(nom = 'Susana', age = 0){
 
-    const greating = 'Hola Mon mundial';
+    const greating = `Hola Mon mundial ${nom} ${age}`;
     // console.log("Hola Jaume");
 
     return greating;
 };
 
+//const result = sayHello();
+//console.log(result);
+console.log(sayHello("Jaume"));
 
-const result = sayHello();
-
-console.log(result);
 
 
 
