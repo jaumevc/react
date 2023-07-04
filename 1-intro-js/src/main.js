e0d678c6644db22030027c1f740b11acd64f5eee
jaumevc <jaumevc@me.com>
2023-07-04 09:08:05 +0200

    video 11

--- 1-intro-js/src/main.js
+++ 1-intro-js/src/main.js
@@ -12,5 +12,35 @@ function sayHello(nom = 'Susana', age = 0){
 console.log(sayHello("Jaume"));
 
 
+//FUNCIO ANÒNIMA:
+const diguesHola = function(nom = 'Susana', age = 0){
+    const greating = `Hola Mon mundial ${nom} ${age}`;
+    // console.log("Hola Jaume");
+    return greating;
+};
+
+let salutacio = diguesHola();
+console.log(salutacio);
+
+//FUNCIÓ FLETXA:
+/*
+const diguesHi = (nom = 'Ares', age = 14) => {
+    const greating = `Hola Mon mundial ${nom} ${age}`;
+    // console.log("Hola Jaume");
+    return greating;
+};
+
+const diguesHi = (nom = 'Ares', age = 14) => {
+    return `Hola Mon mundial ${nom} ${age}`;
+};
+*/
+
+const diguesHi = (nom = 'Mariona', age = 17) =>  `Hola Mon mundial ${nom} ${age}`;
+
+console.log(diguesHi());
+
+const add = (a = 0, b = 0) =>  a+b;
+
+console.log(add(10,5));
 
 
