afbe1445b3f284a7b73dd825dc36af3fd9524c5c
jaumevc <jaumevc@me.com>
2023-07-11 13:42:00 +0200

    video 24 import-export

---
+++ 1-intro-js/src/data/invoice.js
@@ -0,0 +1,89 @@
+
+export const itemReferencia ={product:'gamming chair', price:340, quantity:15};
+
+export const invoices = [
+    {
+        id:1,
+        name: 'Material d\'oficina',
+        date: new Date(),
+        client: {
+            name: 'Corporation & Coo',
+            adress: 'C/Pincipal s/n'
+        },
+        items:[
+            {product:'mouse', price:30, quantity:15},
+            {product:'disk ssd 128gb', price:140, quantity:15},
+            itemReferencia
+        ], 
+        total: function(){
+            let valor = 0;
+
+            this.items.forEach(item =>{
+                valor += item.price * item.quantity;
+            });
+            
+            return valor;
+        },
+        greeting:function() {
+            return `Hola ${this.client.name}`;
+        }
+    },
+
+    {
+        id:2,
+        name: 'Material magatzem',
+        date: new Date(),
+        client: {
+            name: 'Company & Coo',
+            adress: 'C/Major s/n'
+        },
+        items:[
+            {product:'keyboard', price:30, quantity:15},
+            {product:'Screen 15"', price:140, quantity:15},
+            {product:'pc', price:740, quantity:15}
+        ], 
+        total: function(){
+            let valor = 0;
+
+            this.items.forEach(item =>{
+                valor += item.price * item.quantity;
+            });
+            
+            return valor;
+        },
+        greeting:function() {
+            return `Hola ${this.client.name}`;
+        }
+    },
+
+    {
+        id:3,
+        name: 'Material TEC',
+        date: new Date(),
+        client: {
+            name: 'Tecnlogies & Coo',
+            adress: 'C/Vulevart s/n'
+        },
+        items:[
+            {product:'Screen gamming 24"', price:240, quantity:15},
+            {product:'gamming chair', price:340, quantity:15},
+            itemReferencia
+        ], 
+        total: function(){
+            let valor = 0;
+            
+            for(let i =0; i<this.items.length; i++){
+                valor = valor + this.items[i].price * this.items[i].quantity;
+            }
+            
+            return valor;
+        },
+        greeting:function() {
+            return `Hola ${this.client.name}`;
+        }
+    }
+];
+
+export const invoiceByName = (nom) =>{
+    return invoices.find(factura => factura.name === nom);
+}
--- 1-intro-js/src/main.js
+++ 1-intro-js/src/main.js
@@ -1,96 +1,15 @@
-//15-Arrays-find-filter-other-methods
+//16-import-espor-modules
 
-const itemReferencia ={product:'gamming chair', price:340, quantity:15};
-const invoices = [
-    {
-        id:1,
-        name: 'Material d\'oficina',
-        date: new Date(),
-        client: {
-            name: 'Corporation & Coo',
-            adress: 'C/Pincipal s/n'
-        },
-        items:[
-            {product:'mouse', price:30, quantity:15},
-            {product:'disk ssd 128gb', price:140, quantity:15},
-            itemReferencia
-        ], 
-        total: function(){
-            let valor = 0;
+import {invoices, itemReferencia, invoiceByName} from './data/invoice';
 
-            this.items.forEach(item =>{
-                valor += item.price * item.quantity;
-            });
-            
-            return valor;
-        },
-        greeting:function() {
-            return `Hola ${this.client.name}`;
-        }
-    },
-
-    {
-        id:2,
-        name: 'Material magatzem',
-        date: new Date(),
-        client: {
-            name: 'Company & Coo',
-            adress: 'C/Major s/n'
-        },
-        items:[
-            {product:'keyboard', price:30, quantity:15},
-            {product:'Screen 15"', price:140, quantity:15},
-            {product:'pc', price:740, quantity:15}
-        ], 
-        total: function(){
-            let valor = 0;
-
-            this.items.forEach(item =>{
-                valor += item.price * item.quantity;
-            });
-            
-            return valor;
-        },
-        greeting:function() {
-            return `Hola ${this.client.name}`;
-        }
-    },
-
-    {
-        id:3,
-        name: 'Material TEC',
-        date: new Date(),
-        client: {
-            name: 'Tecnlogies & Coo',
-            adress: 'C/Vulevart s/n'
-        },
-        items:[
-            {product:'Screen gamming 24"', price:240, quantity:15},
-            {product:'gamming chair', price:340, quantity:15},
-            itemReferencia
-        ], 
-        total: function(){
-            let valor = 0;
-            
-            for(let i =0; i<this.items.length; i++){
-                valor = valor + this.items[i].price * this.items[i].quantity;
-            }
-            
-            return valor;
-        },
-        greeting:function() {
-            return `Hola ${this.client.name}`;
-        }
-    }
-];
 
 //const invoice =  invoices.find(factura => {return factura.id === 3});
 const invoiceById =  invoices.find(factura => factura.id === 3);
-const invoiceByName =  invoices.find(factura => factura.name === 'Material magatzem');
+//const invoiceByName =   invoices.find(factura => factura.name === 'Material magatzem');
 const invoiceByClientName =  invoices.find(factura => factura.client.name === 'Corporation TURURUT');
 
 console.log(invoiceById);
-console.log(invoiceByName);
+console.log(invoiceByName('Material magatzem'));
 console.log(invoiceByClientName);
 
 //FILTRAR...pels id > 1
