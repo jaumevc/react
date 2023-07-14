f32a2d6e974df2768bbf2bc12e22c6f31a6bc110
jaumevc <jaumevc@me.com>
2023-07-14 11:05:46 +0200

    video 31 api dom

--- 1-intro-js/src/main.js
+++ 1-intro-js/src/main.js
@@ -1,36 +1,25 @@
 //23-Api-dom
 
-//El fetch es una Promise que retorna un response que és una altra Promise per poder convertir-ho a un cert format (xe:json)
-//Enlloc del httpClient tb li podriem dir promise -> ja que es una promesa 
-//const httpClient = fetch('https://jsonplaceholder.typicode.com/users');
-
-/*
-httpClient.then((response)=>{
-    //hem de convertir la promesa a JSON que a la seva vegada retorna una altra promesa
-    response.json().then(data =>{
-        console.log(data);
-    });
-}).catch((reject)=>{
-    console.error(reject);
-});
-*/
-
-/*
-httpClient.then((response)=>{
-    return response.json();
-}).then((data)=>console.log(data));
-*/
 const findAllUsers = async()=>{
     //El fetch es una promesa i amb el await diem que esperem a que es resolgui
     const response = await fetch('https://jsonplaceholder.typicode.com/users');// amb await es converteix a la resposta
     //El json tb es una promesa i tb espeara el resolve de la promesa
-    return await response.json(); //amb el await es converteix al data, al json de resposta
-    //console.log(data);
-    //return data;
+    const users = await response.json(); //amb el await es converteix al data, al json de resposta
+    //crear una llista en html
+    const ul = document.createElement('ul');
+    users.forEach(user => {
+        //cada element de la llista en html
+        const li = document.createElement('li');
+        li.innerText =user.name;
+        ul.append(li);
+
+        console.log(user.name);
+    });
+
+    document.getElementById('root').append(ul);
 }
 
-const users = await findAllUsers();
-console.log(users);
+findAllUsers();
 
 console.log('Mostrant si hi ha delay');
 
