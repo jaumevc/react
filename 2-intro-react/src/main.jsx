424ed45020163de785eccefbac6b6fe3a3c64757
jaumevc <jaumevc@me.com>
2023-07-17 13:10:02 +0200

    video 36 tractament del DOM a react

--- 2-intro-react/src/main.jsx
+++ 2-intro-react/src/main.jsx
@@ -3,11 +3,25 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+const h1 = React.createElement('h1', null, 'Hola Jaume Valls');
+const div = React.createElement('div', null, React.createElement('ul', null, React.createElement('li',null, 'Format en react')));
+
+const seccio= <div><ul><li>Proves React amb tags</li></ul></div>;
+
+const babel = /*#__PURE__*/React.createElement("container", null, 
+              /*#__PURE__*/React.createElement("div", null, 
+              /*#__PURE__*/React.createElement("ul", null, 
+              /*#__PURE__*/React.createElement("li", null, "dada 1"), 
+              /*#__PURE__*/React.createElement("li", null, "dada 2"))));
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <h1>Hola Mon</h1>
-    <div>Hola Mon mundial</div>
-    <App/>
-  </React.StrictMode>,
-  
+  // <React.StrictMode>
+  //   <h1>Hola Mon</h1>
+  //   <div>Hola Mon mundial</div>
+  //   <App/>
+  // </React.StrictMode>,
+  // h1,
+  // div,
+  // seccio
+  babel
 )
