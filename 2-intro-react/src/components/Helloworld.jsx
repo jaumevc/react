cec0bde4fa31e861ef070f092562d0a7ac99fa28
jaumevc <jaumevc@me.com>
2023-07-17 14:27:31 +0200

    video 37 1er component react

---
+++ 2-intro-react/src/components/Helloworld.jsx
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+
+function HelloWorld(){
+    return (
+            <Fragment>
+                <div>Hello Món mundial</div>
+                <h1>Hola que tal aniuat</h1>
+            </Fragment>
+        );
+}
+
+export{
+    HelloWorld 
+}
