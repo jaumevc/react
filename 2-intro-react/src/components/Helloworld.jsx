e655a87f10c5db67b6b563afcc298a2c7b94fb45
jaumevc <jaumevc@me.com>
2023-07-18 09:58:15 +0200

    video 38 variables i parametres en components

--- 2-intro-react/src/components/Helloworld.jsx
+++ 2-intro-react/src/components/Helloworld.jsx
@@ -1,14 +1,26 @@
 import { Fragment } from "react";
 
-function HelloWorld(){
+/*
+function HelloWorld({user, id}){
+    const carrec = 'Boss';
     return (
             <Fragment>
-                <div>Hello Món mundial</div>
                 <h1>Hola que tal aniuat</h1>
+                <div>Hello Món mundial i valor variable = {user}, amb id = {id} i càrrec = {carrec}</div>
             </Fragment>
         );
 }
+*/
 
+const HelloWorld = ({user, id})=>{
+    const carrec = 'Boss';
+    return (
+            <Fragment>
+                <h1>Hola que tal aniuat</h1>
+                <div>Hello Món mundial i valor variable = {user}, amb id = {id} i càrrec = {carrec}</div>
+            </Fragment>
+        );
+}
 export{
     HelloWorld 
 }
