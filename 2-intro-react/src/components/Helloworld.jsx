f28763777ecd08ced286132b5d3d79821a08e6c1
jaumevc <jaumevc@me.com>
2023-07-18 13:11:17 +0200

    video 39 components, atributs i variables

--- 2-intro-react/src/components/Helloworld.jsx
+++ 2-intro-react/src/components/Helloworld.jsx
@@ -12,13 +12,15 @@ function HelloWorld({user, id}){
 }
 */
 
-const HelloWorld = ({user, id})=>{
+const HelloWorld = ({user, id, title = 'Hello mondie!!'})=>{
+   
     const carrec = 'Boss';
     return (
-            <Fragment>
-                <h1>Hola que tal aniuat</h1>
-                <div>Hello Món mundial i valor variable = {user}, amb id = {id} i càrrec = {carrec}</div>
-            </Fragment>
+            <>
+                <h1>{title}</h1>
+                {/* <div>Hello Món mundial i valor variable = {JSON.stringify(user)}, amb id = {id + 50} i càrrec = {carrec}</div>  */}
+                <p>el usuari es diu {user.name} {user.lastName}</p>
+            </>
         );
 }
 export{
