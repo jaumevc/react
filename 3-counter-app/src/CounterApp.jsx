fc80ed6cad368d9d6cbdcf2af4b971ebe87d3ea4
jaumevc <jaumevc@me.com>
2023-07-21 10:59:09 +0200

    video 46 inici contador

--- 3-counter-app/src/CounterApp.jsx
+++ 3-counter-app/src/CounterApp.jsx
@@ -1,3 +1,15 @@
 export const CounterApp = ()=>{
-    return <></>;
+
+    let incremental= 0;
+    const counter = () =>{
+        incremental = incremental+1;
+        console.log(incremental);
+        }
+
+    return <>
+        <h2>El valor del contador és: {incremental}</h2>
+        <button onClick= {()=>{
+            counter();
+        }} >Incrmentar +1</button>
+    </>;
 }
