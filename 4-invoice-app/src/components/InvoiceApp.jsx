137d405322ef54e875a4e5c002ef350c8fc10879
jaumevc <jaumevc@me.com>
2023-07-24 13:04:29 +0200

    video 54 maquetat de dades desestructuracio

--- 4-invoice-app/src/components/InvoiceApp.jsx
+++ 4-invoice-app/src/components/InvoiceApp.jsx
@@ -2,19 +2,40 @@
 import { getInvoice } from "../services/getInvoice";
 
 const InvoiceApp = () =>{
-
-    const invoice = getInvoice();
+    // const invoice = getInvoice();
+    const {id, name, client,company, items} = getInvoice();
+    //xq no hi hagi conflicte amb el name de la fctra li donem un alies (nameClient) 
+    //al nom del client amb name:nameClient
+    const {name:nameClient, lastName, adress} = client;
+    const {country, city, street, number} = adress;
 
     return (
         <>
         <h1>Exemple Factura</h1>
         <ul>
-            <li> Id: {invoice.id} </li>
-            <li> Name: {invoice.name} </li>
+            {/* <li> Id: {invoice.id} </li>
+            <li> Name: {invoice.name} </li> */}
+            <li> Id: {id} </li>
+            <li> Name: {name} </li>
+        </ul>
+        <h3>Dades del client:</h3>
+        <ul>
+            {/* <li>{invoice.client.name} {invoice.client.lastName}</li>
+            <li>{invoice.client.adress.street}, {invoice.client.adress.number}</li>
+            <li>{invoice.client.adress.country}, {invoice.client.adress.city}</li> */}
+            {/* <li>{client.name} {client.lastName}</li>
+            <li>{client.adress.street}, {client.adress.number}</li>
+            <li>{client.adress.country}, {client.adress.city}</li> */}
+            <li>{nameClient} {lastName}</li>
+            <li>{street}, {number}</li>
+            <li>{country}, {city}</li>
+        </ul>
+        <h3>Dades de l'empresa:</h3>
+        <ul>
+            <li>{company.name}, <b>amb cif:</b> {company.cif}</li>
         </ul>
         </>
     );
-
 }
 
 export {
