68dfc0b62826dd2f320d9a34ad4f78d62666cc99
jaumevc <jaumevc@me.com>
2023-07-25 14:25:45 +0200

    video 56.2 bootstrap min 12:24

--- 4-invoice-app/src/components/InvoiceApp.jsx
+++ 4-invoice-app/src/components/InvoiceApp.jsx
@@ -11,71 +11,79 @@ const InvoiceApp = () =>{
 
     return (
         <>
-        <h1>Exemple Factura</h1>
-        <ul>
-            {/* <li> Id: {invoice.id} </li>
-            <li> Name: {invoice.name} </li> */}
-            <li> Id: {id} </li>
-            <li> Name: {name} </li>
-        </ul>
-        <h3>Dades del client:</h3>
-        <ul>
-            {/* <li>{invoice.client.name} {invoice.client.lastName}</li>
-            <li>{invoice.client.adress.street}, {invoice.client.adress.number}</li>
-            <li>{invoice.client.adress.country}, {invoice.client.adress.city}</li> */}
-            {/* <li>{client.name} {client.lastName}</li>
-            <li>{client.adress.street}, {client.adress.number}</li>
-            <li>{client.adress.country}, {client.adress.city}</li> */}
-            <li>{nameClient} {lastName}</li>
-            <li>{street}, {number}</li>
-            <li>{country}, {city}</li>
-        </ul>
-        <h3>Dades de l'empresa:</h3>
-        <ul>
-            <li>{company.name}, <b>amb cif:</b> {company.cif}</li>
-        </ul>
-        <h4>Línies de detall de la factura:</h4>
-        <table>
-            <thead>
-                <tr>
-                    <th>Product</th>
-                    <th>Quantity</th>
-                    <th>Price</th>
-                </tr>
-            </thead>
-            <tbody>
-                {/* {items.map(item =>{
-                    return(
+            <div className="container">
+                <h1>Exemple Factura</h1>
+                <ul className="list-group">
+                    {/* <li> Id: {invoice.id} </li>
+                    <li> Name: {invoice.name} </li> */}
+                    <li className="list-group-item"> Id: {id} </li>
+                    <li className="list-group-item"> Name: {name} </li>
+                </ul>
+                <div className="row">
+                    <div className="col">
+                        <h3>Dades del client:</h3>
+                        <ul className="list-group">
+                            {/* <li>{invoice.client.name} {invoice.client.lastName}</li>
+                            <li>{invoice.client.adress.street}, {invoice.client.adress.number}</li>
+                            <li>{invoice.client.adress.country}, {invoice.client.adress.city}</li> */}
+                            {/* <li>{client.name} {client.lastName}</li>
+                            <li>{client.adress.street}, {client.adress.number}</li>
+                            <li>{client.adress.country}, {client.adress.city}</li> */}
+                            <li className="list-group-item">{nameClient} {lastName}</li>
+                            <li className="list-group-item">{street}, {number}</li>
+                            <li className="list-group-item">{country}, {city}</li>
+                        </ul>
+                    </div>
+                    <div className="col">
+                        <h3>Dades de l'empresa:</h3>
+                        <ul className="list-group">
+                            <li className="list-group-item">{company.name}, <b>amb cif:</b> {company.cif}</li>
+                        </ul>
+                    </div>
+                </div>
+            
+                <h4>Línies de detall de la factura:</h4>
+                <table className="table table-striped table-hover">
+                    <thead>
                         <tr>
-                            <td>{item.product}</td>
-                            <td>{item.quantity}</td>
-                            <td>{item.price}</td>
+                            <th>Product</th>
+                            <th>Quantity</th>
+                            <th>Price</th>
                         </tr>
-                        );
-                    }
-                )} */}
+                    </thead>
+                    <tbody>
+                        {/* {items.map(item =>{
+                            return(
+                                <tr>
+                                    <td>{item.product}</td>
+                                    <td>{item.quantity}</td>
+                                    <td>{item.price}</td>
+                                </tr>
+                                );
+                            }
+                        )} */}
 
-                {/* {items.map(({product, quantity, price}) =>{
-                    return(
-                        <tr>
-                            <td>{product}</td>
-                            <td>{quantity}</td>
-                            <td>{price}</td>
-                        </tr>
-                        );
-                    }
-                )} */}
+                        {/* {items.map(({product, quantity, price}) =>{
+                            return(
+                                <tr>
+                                    <td>{product}</td>
+                                    <td>{quantity}</td>
+                                    <td>{price}</td>
+                                </tr>
+                                );
+                            }
+                        )} */}
 
-                {items.map(({id, product, quantity, price}) => (
-                        <tr key={id}>
-                            <td>{product}</td>
-                            <td>{quantity}</td>
-                            <td>{price}</td>
-                        </tr>)
-                        )}
-            </tbody>
-        </table>
-
+                        {items.map(({id, product, quantity, price}) => (
+                                <tr key={id}>
+                                    <td>{product}</td>
+                                    <td>{quantity}</td>
+                                    <td>{price}</td>
+                                </tr>)
+                                )}
+                    </tbody>
+                </table>
+            </div>
         </>
     );
 }
