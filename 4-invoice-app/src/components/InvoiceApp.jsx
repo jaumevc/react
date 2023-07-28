b99242ad1f2462b95f5069ec196d0f22dfc0645c
jaumevc <jaumevc@me.com>
2023-07-28 12:42:55 +0200

    neteja codi comentat en  invoiceApp.jsx

--- 4-invoice-app/src/components/InvoiceApp.jsx
+++ 4-invoice-app/src/components/InvoiceApp.jsx
@@ -1,4 +1,3 @@
-//  import { invoice } from "../data/invoice";
 import { getInvoice } from "../services/getInvoice";
 import { InvoiceClientData } from "./InvoiceClientData";
 import { InvoiceDataRef } from "./invoiceDataRef";
@@ -8,125 +7,45 @@ import { InvoiceDetail } from "./InvoiceDetail";
 
 
 const InvoiceApp = () => {
-    // const invoice = getInvoice();
     const { id, name, client, company, items } = getInvoice();
-    //xq no hi hagi conflicte amb el name de la fctra li donem un alies (nameClient) 
-    //al nom del client amb name:nameClient
-    // const { name: nameClient, lastName, adress } = client;
-    // const { country, city, street, number } = adress;
 
     return (
         <>
             <div className="container">
                 <div className="card my-5">
-                    {/* <h1>Exemple Factura</h1> */}
+
                     <div className="card-header text-center">
                         <h3>Exemple Factura</h3>
                     </div>
                     <div className="card-body">
 
-                        <InvoiceDataRef id={id} name = {name}/>
-
-                        {/* <ul className="list-group">
-                            {/* <li> Id: {invoice.id} </li>
-                            <li> Name: {invoice.name} </li> */}
-                        {/* <li className="list-group-item"> Id: {id} </li>
-                            <li className="list-group-item"> Name: {name} </li>
-                        </ul> */}
+                        <InvoiceDataRef id={id} name={name} />
 
                         <div className="row my-3">
                             <div className="col">
-                                {/* <h3>Dades del client:</h3> */}
-                                
-                                {/* <InvoiceClientData 
-                                    nameClient = {nameClient}
-                                    lastName = {lastName}
-                                    country = {country}
-                                    city = {city}
-                                    street = {street}
-                                    number = {number}
-                                /> */}
 
-                                <InvoiceClientData 
-                                    title = "Dades del client:"
-                                    client = {client}
+                                <InvoiceClientData
+                                    title="Dades del client:"
+                                    client={client}
                                 />
-
-                                {/* <ul className="list-group"> */}
-                                    {/* <li>{invoice.client.name} {invoice.client.lastName}</li>
-                            <li>{invoice.client.adress.street}, {invoice.client.adress.number}</li>
-                            <li>{invoice.client.adress.country}, {invoice.client.adress.city}</li> */}
-                                    {/* <li>{client.name} {client.lastName}</li>
-                            <li>{client.adress.street}, {client.adress.number}</li>
-                            <li>{client.adress.country}, {client.adress.city}</li> */}
-                                    {/* <li className="list-group-item active">{nameClient} {lastName}</li>
-                                    <li className="list-group-item">{street}, {number}</li>
-                                    <li className="list-group-item">{country}, {city}</li>
-                                </ul> */}
-
 
                             </div>
                             <div className="col">
-                                {/* <h3>Dades de l'empresa:</h3> */}
 
                                 <InvoiceEnterprisseData
-                                    title = "Dades de l'empresa:"
-                                    company = {company}
+                                    title="Dades de l'empresa:"
+                                    company={company}
                                 />
-
-                                {/* <ul className="list-group">
-                                    <li className="list-group-item active">{company.name}</li>
-                                    <li className="list-group-item"><b>amb cif:</b> {company.cif}</li>
-                                </ul> */}
                             </div>
                         </div>
 
-                        <h4>Línies de detall de la factura:</h4>
+                        <div>
+                            <InvoiceDetail
+                                title = "Línies de detall de la factura:"
+                                items={items}
+                            />
+                        </div>
 
-                        <InvoiceDetail 
-                             items = {items}
-                        />
-
-                        {/* <table className="table table-striped table-hover">
-                            <thead>
-                                <tr>
-                                    <th>Product</th>
-                                    <th>Quantity</th>
-                                    <th>Price</th>
-                                </tr>
-                            </thead>
-                            <tbody>  */}
-                                {/* {items.map(item =>{
-                            return(
-                                <tr>
-                                    <td>{item.product}</td>
-                                    <td>{item.quantity}</td>
-                                    <td>{item.price}</td>
-                                </tr>
-                                );
-                            }
-                        )}
-
-                                {/* {items.map(({product, quantity, price}) =>{
-                            return(
-                                <tr>
-                                    <td>{product}</td>
-                                    <td>{quantity}</td>
-                                    <td>{price}</td>
-                                </tr>
-                                );
-                            }
-                        )} */}
-{/* 
-                                {items.map(({ id, product, quantity, price }) => (
-                                    <tr key={id}>
-                                        <td>{product}</td>
-                                        <td>{quantity}</td>
-                                        <td>{price}</td>
-                                    </tr>)
-                                )}
-                            </tbody>
-                        </table> */}
                     </div>
                 </div>
             </div>
--- 4-invoice-app/src/components/InvoiceDetail.jsx
+++ 4-invoice-app/src/components/InvoiceDetail.jsx
@@ -1,6 +1,7 @@
-export const InvoiceDetail = ({items}) => {
+export const InvoiceDetail = ({title, items}) => {
     return (
         <>
+            <h4>{title}</h4>
             <table className="table table-striped table-hover">
                 <thead>
                     <tr>
