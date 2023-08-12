20db0a13f738c4f01ac4a1732f72e86bb8ffd14e
jaumevc@me.com <jaumevc@me.com>
2023-08-12 17:48:08 +0200

    V78 maquetacio-1, html css

--- 5-cart-app/src/CartApp.jsx
+++ 5-cart-app/src/CartApp.jsx
@@ -1,7 +1,71 @@
 export const  CartApp = () =>{
     return (
         <>
-            <h3>hello Cistella de compres</h3>
+            <div className="container">
+                <h3>Cistella de Compres</h3>
+                <div className="row">
+                    <div className="col-4 my-2">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Teclat Mecànic RGB</h5>
+                                <p className="cart-text">Teclat mecànic retro il·luminat amb colors RGG</p>
+                                <p className="cart-text">250,00 €</p>
+                                <button className="btn btn-primary">Afegir a la Cistella</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-4 my-2">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Samsung Smart TV 50"</h5>
+                                <p className="cart-text">Televisor alta definició i so envolvent</p>
+                                <p className="cart-text">950,00 €</p>
+                                <button className="btn btn-primary">Afegir a la Cistella</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-4 my-2">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Memoria Ram 32 GB</h5>
+                                <p className="cart-text">Memoria Ramb de 32 GB de capacitat</p>
+                                <p className="cart-text">180,00 €</p>
+                                <button className="btn btn-primary">Afegir a la Cistella</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-4 my-2">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Màgic Mouse</h5>
+                                <p className="cart-text">Mouse alta gama</p>
+                                <p className="cart-text">120,00 €</p>
+                                <button className="btn btn-primary">Afegir a la Cistella</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-4 my-2">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Pantalla HP 24"</h5>
+                                <p className="cart-text">Pantalla alta definició HP</p>
+                                <p className="cart-text">230,00 €</p>
+                                <button className="btn btn-primary">Afegir a la Cistella</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-4 my-2">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Disc SSD</h5>
+                                <p className="cart-text">Disc SSD 250 GB</p>
+                                <p className="cart-text">140,00 €</p>
+                                <button className="btn btn-primary">Afegir a la Cistella</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
