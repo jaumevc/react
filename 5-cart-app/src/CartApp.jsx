8769b739cfd14809815b8641b0ad894edf607a91
jaumevc@me.com <jaumevc@me.com>
2023-08-14 11:11:08 +0200

    V82 Service + Hook useEffect + useState

--- 5-cart-app/src/CartApp.jsx
+++ 5-cart-app/src/CartApp.jsx
@@ -1,69 +1,40 @@
-export const  CartApp = () =>{
+import { useEffect, useState } from "react";
+import { getProducts } from "./services/productService";
+
+export const CartApp = () => {
+
+    // const products= getProducts();
+    // es carregarà un array vuit en productes 
+    const [products, setProducts] = useState([]);
+    // en el useEffect es carregaran les dades a la const products
+    useEffect(
+        () => {
+            setProducts(getProducts());
+        }, []
+    );
+
     return (
         <>
             <div className="container">
                 <h3>Productes</h3>
                 <div className="row">
-                    <div className="col-4 my-2">
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">Teclat Mecànic RGB</h5>
-                                <p className="cart-text">Teclat mecànic retro il·luminat amb colors RGG</p>
-                                <p className="cart-text">250,00 €</p>
-                                <button className="btn btn-primary">Afegir a la Cistella</button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-4 my-2">
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">Samsung Smart TV 50"</h5>
-                                <p className="cart-text">Televisor alta definició i so envolvent</p>
-                                <p className="cart-text">950,00 €</p>
-                                <button className="btn btn-primary">Afegir a la Cistella</button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-4 my-2">
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">Memoria Ram 32 GB</h5>
-                                <p className="cart-text">Memoria Ramb de 32 GB de capacitat</p>
-                                <p className="cart-text">180,00 €</p>
-                                <button className="btn btn-primary">Afegir a la Cistella</button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-4 my-2">
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">Màgic Mouse</h5>
-                                <p className="cart-text">Mouse alta gama</p>
-                                <p className="cart-text">120,00 €</p>
-                                <button className="btn btn-primary">Afegir a la Cistella</button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-4 my-2">
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">Pantalla HP 24"</h5>
-                                <p className="cart-text">Pantalla alta definició HP</p>
-                                <p className="cart-text">230,00 €</p>
-                                <button className="btn btn-primary">Afegir a la Cistella</button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-4 my-2">
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">Disc SSD</h5>
-                                <p className="cart-text">Disc SSD 250 GB</p>
-                                <p className="cart-text">140,00 €</p>
-                                <button className="btn btn-primary">Afegir a la Cistella</button>
-                            </div>
-                        </div>
-                    </div>
+                    {
+                        products.map(
+                            producte => (
+                                <div className="col-4 my-2" key={producte.id}>
+                                    <div className="card">
+                                        <div className="card-body">
+                                            <h5 className="card-title">{producte.name}</h5>
+                                            <p className="cart-text">{producte.description}</p>
+                                            <p className="cart-text">{producte.price}</p>
+                                            <button className="btn btn-primary">Afegir a la Cistella</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        )
+                    }
+                    
                 </div>
                 <div className="my-4 w-50">
                     <h3>Afegit a Cistella</h3>
