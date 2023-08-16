399373050923bf27fb943ce7b25e5eb52ff016c6
jaumevc <jaumevc@me.com>
2023-08-16 14:06:39 +0200

    V84 Estat Cistella de compres

--- 5-cart-app/src/CartApp.jsx
+++ 5-cart-app/src/CartApp.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
 import { CartView } from "./components/CartView";
 import { CatalogView } from "./components/CatalogView";
 
+const initialCartItems = [
+    // {
+    //     product:{},
+    //     quantity: 0,
+    //     total: 0
+    // }
+];
+
 export const CartApp = () => {
+
+    const [cartItems, setCartItems] = useState(initialCartItems);
 
     return (
         <>
@@ -10,7 +21,7 @@ export const CartApp = () => {
                 <CatalogView/>
                 <div className="my-4 w-50">
                     <h3>Afegit a Cistella</h3>
-                    <CartView/>
+                    <CartView items = { cartItems }/>
                 </div>
             </div>
         </>
--- 5-cart-app/src/components/CartView.jsx
+++ 5-cart-app/src/components/CartView.jsx
@@ -1,5 +1,5 @@
 
-export const CartView = () => {
+export const CartView = ({ items }) => {
     return (
         <>
             <table className="table table-hover table-striped">
@@ -13,13 +13,17 @@ export const CartView = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>producte</td>
-                        <td>preu</td>
-                        <td>quantitat</td>
-                        <td>subtotal</td>
-                        <td>eliminar</td>
-                    </tr>
+                    {
+                        items.map(item => (
+                            <tr key={item.product.id}>
+                                <td>{item.product.name}</td>
+                                <td>{item.product.price}</td>
+                                <td>{item.quantity}</td>
+                                <td>{item.quantity * item.product.price}</td>
+                                <td>eliminar</td>
+                            </tr>
+                        ))
+                    }
                 </tbody>
                 <tfoot>
                     <tr>
