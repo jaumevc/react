d925b600b2d67723a0bf8e59d05335ca7bfa8438
jaumevc@me.com <jaumevc@me.com>
2023-08-31 18:20:57 +0200

    V105 maquetació formulari

--- 6-users-app/src/components/UserForm.jsx
+++ 6-users-app/src/components/UserForm.jsx
@@ -1,9 +1,64 @@
+import { useState } from "react";
+
+const initialUserForm = {
+    username:'',
+    password:'',
+    email:''
+}
 export const UserForm = ()=>{
+
+    const [userForm, setUserForm] = useState(initialUserForm);
+
+    const { username, password, email} = userForm;
+
+    const onInputChange = (event)=>{
+        // console.log(event.target.value);
+        setUserForm({
+            ...userForm,
+            [event.target.name]:event.target.value
+        })
+    }
+    //també es pot desestructurar i funciona igual:
+    // const onInputChange = ({target})=>{
+    //     // console.log(target.value);
+    //     const {name, value } = target;
+    //     setUserForm({
+    //         ...userForm,
+    //         [name]:value
+    //     })
+    // }
+
     return(
         <>
             <p>Formulari d'Usuari</p>
             <form>
-
+                <input 
+                    className="form-control my-3 w-75" 
+                    placeholder="Username"
+                    name="username"
+                    value= {username}
+                    onChange={ onInputChange } 
+                />
+                <input 
+                    className="form-control my-3 w-75" 
+                    placeholder="Password"
+                    name="password"
+                    type="password"
+                    value= {password}
+                    onChange={ onInputChange } 
+                />
+                <input 
+                    className="form-control my-3 w-75" 
+                    placeholder="Email"
+                    name="email" 
+                    value= {email}
+                    onChange={ onInputChange } 
+                />
+                <button 
+                    type="submit"
+                    className="btn btn-primary">
+                        Afegir
+                </button>
             </form>
         </>
     );
